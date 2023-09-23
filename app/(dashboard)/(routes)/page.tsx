5e02417eac51fd0180ef396164b80e3d452404c8
Olliveer <j.oliveira90@live.com>
2023-09-23 15:21:37 -0300

    feat: add auth with clerk.dev

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p className="text-red-600">Hello world</p>;
+  return (
+    <div className="">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
