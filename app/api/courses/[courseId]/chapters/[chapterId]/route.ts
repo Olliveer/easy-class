31f26e602fcb286e57d067cb9d8521668aaa0321
Olliveer <j.oliveira90@live.com>
2023-12-06 22:31:38 -0300

    feat: add video upload functionality

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
 import { db } from "@/lib/db";
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID as string,
+  process.env.MUX_TOKEN_SECRET as string
+);
 
 export async function PATCH(
   req: Request,
@@ -42,7 +48,38 @@ export async function PATCH(
       },
     });
 
-    // TODO: video upload
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: "public",
+        test: true,
+      });
+
+      console.log("asset", asset);
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0].id,
+        },
+      });
+    }
 
     return NextResponse.json(chapter);
   } catch (error) {
