e51cc9b29e2e66dde5ea4a955cd64cf756e0f791
Olliveer <j.oliveira90@live.com>
2023-12-06 22:31:53 -0300

    refactor: file router in core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,18 +1,22 @@
 import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
+// import { isTeacher } from "@/lib/teacher";
+
 const f = createUploadthing();
 
 const handleAuth = () => {
   const { userId } = auth();
+  // const isAuthorized = isTeacher(userId);
+
+  console.log("userId", userId);
+
   if (!userId) {
     throw new Error("Unauthorized");
   }
-
   return { userId };
 };
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
@@ -20,29 +24,9 @@ export const ourFileRouter = {
   courseAttachment: f(["text", "image", "video", "audio", "pdf"])
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
-  chapterVideo: f({ video: { maxFileSize: "128GB", maxFileCount: 1 } })
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
-
-//  // Define as many FileRoutes as you like, each with a unique routeSlug
-//  imageUploader: f({ image: { maxFileSize: "4MB" } })
-//  // Set permissions and file types for this FileRoute
-//  .middleware(async ({ req }) => {
-//    // This code runs on your server before upload
-//    const user = await auth(req);
-
-//    // If you throw, the user will not be able to upload
-//    if (!user) throw new Error("Unauthorized");
-
-//    // Whatever is returned here is accessible in onUploadComplete as `metadata`
-//    return { userId: user.id };
-//  })
-//  .onUploadComplete(async ({ metadata, file }) => {
-//    // This code RUNS ON YOUR SERVER after upload
-//    console.log("Upload complete for userId:", metadata.userId);
-
-//    console.log("file url", file.url);
-//  }),
