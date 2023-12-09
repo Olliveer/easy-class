0c236a3c6e829889619fc8e3a68fbd727ce81292
Olliveer <j.oliveira90@live.com>
2023-12-09 16:03:46 -0300

    feat: add publish, unpublish, and delete routes for chapters

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -8,6 +8,94 @@ const { Video } = new Mux(
   process.env.MUX_TOKEN_SECRET as string
 );
 
+export async function DELETE(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      courseId: string;
+      chapterId: string;
+    };
+  }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const ownerCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownerCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse("Not found", { status: 404 });
+    }
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const deleteChapter = await db.chapter.delete({
+      where: {
+        id: chapter.id,
+      },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deleteChapter);
+  } catch (error) {
+    console.log("[ERROR_ROUTE_CHAPTER_DELETE]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   {
@@ -69,8 +157,6 @@ export async function PATCH(
         playback_policy: "public",
         test: true,
       });
-
-      console.log("asset", asset);
 
       await db.muxData.create({
         data: {
