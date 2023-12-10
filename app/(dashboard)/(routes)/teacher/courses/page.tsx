ebc1add3c239778307d75ab8e441bfa50c06d9b0
Olliveer <j.oliveira90@live.com>
2023-12-10 20:42:19 -0300

    feat: add data table component and fetch courses for teacher dashboard

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,44 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { Course } from "@prisma/client";
 
-function Courses() {
+async function getData(userId: string): Promise<Course[]> {
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      isPublished: true,
+      createdAt: true,
+    },
+  });
+
+  return courses as Course[];
+}
+
+async function Courses() {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const data = await getData(userId);
+
   return (
-    <div>
-      <Link href="/teacher/create">
-        <Button>New course</Button>
-      </Link>
+    <div className="p-4">
+      <DataTable
+        columns={columns}
+        data={data}
+      />
     </div>
   );
 }
