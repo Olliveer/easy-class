1e026ae864021b87536a590b7b88f67d599d27c2
Olliveer <j.oliveira90@live.com>
2023-12-10 20:33:44 -0300

    feat: add ConfettiProvider and Toaster components to layout

---
+++ components/providers/confetti-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+import ReactConfetti from "react-confetti";
+
+function ConfettiProvider() {
+  const confetti = useConfettiStore();
+
+  if (!confetti.confetti) return null;
+
+  return (
+    <ReactConfetti
+      className="pointer-events-none z-[100]"
+      numberOfPieces={500}
+      recycle={false}
+      onConfettiComplete={() => confetti.toggleConfetti()}
+    />
+  );
+}
+
+export default ConfettiProvider;
