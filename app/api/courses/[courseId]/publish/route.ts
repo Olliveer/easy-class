a336219ba216bdc1fba65fd9d244bced6421ddc8
Olliveer <j.oliveira90@live.com>
2023-12-10 20:43:19 -0300

    feat: add publish and unpublish routes for courses

---
+++ app/api/courses/[courseId]/publish/route.ts
@@ -0,0 +1,63 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      courseId: string;
+    };
+  }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+      },
+      include: {
+        chapters: true,
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Not found", { status: 404 });
+    }
+
+    const hasPublishedChapters = course.chapters.some(
+      (chapter) => chapter.isPublished
+    );
+
+    if (
+      !course.title ||
+      !course.description ||
+      !course.imageUrl ||
+      !hasPublishedChapters ||
+      !course.categoryId
+    ) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    const publishedCourse = await db.course.update({
+      where: {
+        id: params.courseId,
+      },
+      data: {
+        isPublished: true,
+      },
+    });
+
+    return NextResponse.json(publishedCourse);
+  } catch (error) {
+    console.error("[ERROR_ROUTE_COURSE_PUBLISH]", error);
+    return new NextResponse("Something went wrong", { status: 500 });
+  }
+}
