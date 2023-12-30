a8f88e97a6f7093916f5181c079d678597db1014
Olliveer <j.oliveira90@live.com>
2023-12-30 15:50:26 -0300

    feat: add course enrollment functionality

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 import { formatCurrency } from "@/lib/utils";
+import axios from "axios";
+import { useState } from "react";
 
 type CourseEnrollButtonProps = {
   courseId: string;
@@ -9,10 +12,32 @@ type CourseEnrollButtonProps = {
 };
 
 function CourseEnrollButton({ courseId, price }: CourseEnrollButtonProps) {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong, please try again later",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Button
       className="w-full md:w-auto"
       size={"sm"}
+      onClick={handleClick}
+      disabled={isLoading}
     >
       Enroll for {formatCurrency(price)}
     </Button>
